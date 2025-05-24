9f3ebdbe1e8487c0c3cc1b5634b6e43702cc63bb
Othman Ahmed Yahya <107957606+OthmanYahya@users.noreply.github.com>
2025-05-24 16:26:39 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import { useState, useEffect } from "react";
+import Search from "./components/Search";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET", // HTTP method for requesting the data
+  headers: {
+    accept: "application/json", // Client's response preference "JSON"
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [moviesList, setMoviesList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+
+    // Fetching the data from the api and dealing with api errors
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+
+      // Checks for specific error responses from the API.
+      if (data.Response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+        setMoviesList([]);
+        return;
+      }
+
+      setMoviesList(data.results || []);
+    } catch (error) {
+      // Checking if there is a network/server error
+      console.error("Error fetching movies:", error);
+      setErrorMessage(
+        error.message || "Network error or problem reaching the server"
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="Hero Banner" />
+          <h2>
+            The <span className="text-gradient">Movie Compass</span> is your
+            one-stop guide to your favorite movies and more
+          </h2>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {/* conditional rendering @1:19:40 */}
+          {isLoading ? (
+            <p className="text-white">Loading...</p>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {moviesList.map((movie) => (
+                <p key={movie.id} className="text-white">
+                  {movie.title}
+                </p>
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
